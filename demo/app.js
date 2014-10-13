0b6845503bc998a20cfd732cccf212575ad70f6b
brandly <brandly13@gmail.com>
2014-10-13 15:25:15 -0400

    adjust sorting threshold with a slider

--- demo/app.js
+++ demo/app.js
@@ -4,29 +4,66 @@ var canvas = document.getElementById('sort-pixels'),
     modeSelect = document.getElementById('mode-select'),
     resetButton = document.getElementById('reset-img'),
     butterButton = document.getElementById('buttery'),
+    threshold = document.getElementById('threshold'),
     butter = new Butter(),
     img;
 
+var thresholdSettings = {
+  black: {
+    value: -10000000,
+    min: -20000000,
+    max: -1000000
+  },
+  bright: {
+    value: 30,
+    min: 0,
+    max: 100
+  },
+  white: {
+    value: -6000000,
+    min: -16000000,
+    max: -3000000
+  }
+};
+
 function setImage(uri){
   img = new Image();
-  img.onload = render
+  img.onload = renderImage
   img.src = uri;
 }
 
-function render() {
+function renderImage() {
   canvas.width = 800;
   canvas.height = 600;
   context.drawImage(img, 0, 0);
 }
 
+function getThreshold() {
+  return parseInt(threshold.value, 10);
+}
+
+function updateThresholdRange() {
+  ['min', 'max', 'value'].forEach(function (setting) {
+    threshold[setting] = thresholdSettings[butter.mode][setting];
+  });
+}
+
+updateThresholdRange();
 setImage('wave.jpg');
 
 modeSelect.addEventListener('change', function () {
   butter.mode = modeSelect.value;
+  updateThresholdRange();
 });
 
+threshold.addEventListener('input', function () {
+  var newValue = getThreshold();
+  butter.setThreshold(newValue);
+  thresholdSettings[butter.mode] = newValue;
+})
+
 resetButton.addEventListener('click', function () {
-  render();
+  renderImage();
 });
 
 butterButton.addEventListener('click', function () {
