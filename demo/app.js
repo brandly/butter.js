e935b997c76d00d91b0adf3102d8451bd452437e
brandly <brandly13@gmail.com>
2014-10-13 14:46:28 -0400

    reset button

--- demo/app.js
+++ demo/app.js
@@ -2,21 +2,20 @@
 var canvas = document.getElementById('sort-pixels'),
     context = canvas.getContext('2d'),
     modeSelect = document.getElementById('mode-select'),
-    button = document.getElementById('buttery'),
-    butter = new Butter();
+    resetButton = document.getElementById('reset-img'),
+    butterButton = document.getElementById('buttery'),
+    butter = new Butter(),
+    img;
 
 function setImage(uri){
   img = new Image();
-  img.onload = function () {
-    render(img);
-  }
+  img.onload = render
   img.src = uri;
 }
 
-function render(img) {
+function render() {
   canvas.width = 800;
   canvas.height = 600;
-
   context.drawImage(img, 0, 0);
 }
 
@@ -26,6 +25,10 @@ modeSelect.addEventListener('change', function () {
   butter.mode = modeSelect.value;
 });
 
-button.addEventListener('click', function () {
+resetButton.addEventListener('click', function () {
+  render();
+});
+
+butterButton.addEventListener('click', function () {
   butter.sort(canvas);
 });
