840eaef713a10ec62decee145449fc40dabefae0
brandly <brandly13@gmail.com>
2014-10-14 00:04:58 -0400

    alert size

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function setImageURL(url){
 function renderImage() {
   canvas.width = img.width;
   canvas.height = img.height;
-  alert('RENDERING IMAGE');
+  alert('IMG SIZE: ' + img.width + ' x ' + img.height);
   context.drawImage(img, 0, 0);
 }
 
