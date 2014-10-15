7760699588a914156ef2b45f446a626a8c9e116b
brandly <brandly13@gmail.com>
2014-10-14 22:14:27 -0400

    use <img> for final viewing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-var canvas = document.getElementById('sort-pixels'),
+var canvas = document.createElement('canvas'),
     context = canvas.getContext('2d'),
+    destination = document.getElementById('sort-pixels'),
     fileInput = document.getElementById('file-upload'),
     modeSelect = document.getElementById('mode-select'),
     resetButton = document.getElementById('reset-img'),
@@ -44,6 +45,8 @@ function renderImage() {
 
   context.drawImage(img, 0, 0, width, height);
   img.onload = null;
+
+  destination.src = canvas.toDataURL("image/png");
 }
 
 function getThreshold() {
